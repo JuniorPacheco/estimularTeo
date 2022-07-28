9a19e58370bbf75a3a98622c7e18f247fb0020c1
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-07-28 18:36:22 -0500

    Cambios al logo y a textos

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         <section className='header'>
           <div className='header__logo'>
             <div className='logo__image'>
-              <img src="/logoCompleto.png" alt="logo" />
+              <img src="/logo2.png" alt="logo" />
             </div>
           </div>
           <div className='header__items'>
@@ -46,17 +46,27 @@ export default function Home() {
             <img src="https://images.unsplash.com/photo-1545803928-04e3f4cdd4ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="imagen personal" />
           </div>
           <section className='inicio__profesion'>
-            <p>Terapeuta Ocupacional y Creadora de EstimularTeo</p>
-            <p>Certificación en integración sensorial</p>
+            <p>Soy Terapeuta Ocupacional y alcanzar el máximo nivel de autonomía, funcionalidad y 
+              participación en las actividades de la vida diaria de una persona en las diferentes 
+              etapas de la vida es mi trabajo.</p>
             <h1>Andrea Bohorquez</h1>
           </section>
         </div>
+        <h2>¿Qué hace un terapeuta ocupacional en la escuela?</h2>
         <p className='inicio__textoPrincipal'>
-        It is a long established fact that a reader will be distracted by the readable content of a page when 
-        looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution 
-        of letters, as opposed to using Content here, content here, making it look like readable English. 
-        Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
-        and a search for lorem ipsum will uncover many web sites still in their infancy. 
+        El rol ocupacional del niño centra sus actividades en la escuela, es por esto que un 
+        terapeuta ocupacional en dicho entorno es un profesional que puede apoyar las adaptaciones 
+        razonables del currículo que el profesor del aula realiza a través del PIAR, para aquellos 
+        niños que lo requieran, de modo que, además de asistir a la clase, también alcancen su máximo 
+        nivel de participación, inclusión y rendimiento en las actividades del aula. 
+        El terapeuta ocupacional será una parte vital del equipo que trabaja con los niños que tienen 
+        necesidades educativas especiales y requieren apoyo adicional para lograr mejores resultados 
+        en la escuela, ya que se encargará de identificar la base de las dificultades de cada 
+        niño a nivel de neurodesarrollo y desarrollo sensoriomotor determinando el impacto en lo 
+        relacionado con el movimiento, la postura, la comunicación y el aprendizaje, corrigiendo y 
+        previniendo la aparición de mayores dificultades que puedan generarse; lo hará través de observaciones 
+        y razonamientos clínicos; proporcionando recomendaciones al profesor encargado para que pueda aplicarlas 
+        en el aula.
         </p>
       </section>
       <section className='servicios'>
